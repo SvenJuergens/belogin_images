014c03f100fb118f9a5e375499ef1988a2e28c6a
Sven Juergens <sj@nordsonne.de>
2020-02-28 13:54:06 +0100

    [TASK] compatibility with TYPO3 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '8.7.0-10.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
