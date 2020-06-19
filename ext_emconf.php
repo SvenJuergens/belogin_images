5c93910e083095f21f2287140820f9abcda048c3
SvenJuergens <SvenJuergens@users.noreply.github.com>
2020-06-19 12:00:31 +0200

    [TASK] Raise requirements for TYPO3 core

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '9.5.17-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
